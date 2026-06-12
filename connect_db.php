cf4a83c17d79b83f7a22681721498e8cb5dd1e44
KZChan0415 <chan2311967@gmail.com>
2026-06-12 21:22:14 +0800

    connect postgresql database

--- connect_db.php
+++ connect_db.php
@@ -1,16 +1,16 @@
 <?php
 session_start();
 
-$servername = "localhost";
-$port = "5432";
+$servername = "localhost"; 
 $username = "postgres";
 $password = "CPLAssets123";
 $dbname = "asset_system";
+$port = "5432";
 
 try {
     $pdo = new PDO("pgsql:host=$servername;port=$port;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (\PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
